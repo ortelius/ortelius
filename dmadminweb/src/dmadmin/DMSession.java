a6badd15ac77ac7d804341051560d7be6f51cbd0
Steve Taylor <steve@deployhub.com>
2020-10-08 15:32:37 -0600

    Fix compile errors Signed-off-by: Steve Taylor <steve@deployhub.com>

--- dmadminweb/src/dmadmin/DMSession.java
+++ dmadminweb/src/dmadmin/DMSession.java
@@ -53,6 +53,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.Enumeration;
--- dmadminweb/src/dmadmin/model/Engine.java
+++ dmadminweb/src/dmadmin/model/Engine.java
@@ -18,6 +18,7 @@
  */
 package dmadmin.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
