a8f461c89aa2959d1e58bf443e5fef35df3f89c3
piyush94 <piyush.sohal94@gmail.com>
2021-06-17 18:31:09 +0530

    indentation fix

--- installers/linux/engine/scripts/dh2helm.py
+++ installers/linux/engine/scripts/dh2helm.py
@@ -363,13 +363,13 @@ def main():
         if ('helmrepouser' in newvals):
             mylogin = mylogin + "--username " + newvals['helmrepouser'] + " "
 
-		if ('helmrepopass' in newvals):
+        if ('helmrepopass' in newvals):
             mylogin = mylogin + "--password " + newvals['helmrepopass'] + " "
-			
-		if (helm_exe.lower() == "helm2"):
-			runcmd(fp_task, to_dir, helm_exe + ' repo add ' + mylogin + newvals['helmrepo']['name'] + " " + newvals['helmrepo']['url'])
-		else:
-			runcmd(fp_task, to_dir, helm_exe + ' repo add --force-update ' + mylogin + newvals['helmrepo']['name'] + " " + newvals['helmrepo']['url'])
+
+        if (helm_exe.lower() == "helm2"):
+            runcmd(fp_task, to_dir, helm_exe + ' repo add ' + mylogin + newvals['helmrepo']['name'] + " " + newvals['helmrepo']['url'])
+        else:
+            runcmd(fp_task, to_dir, helm_exe + ' repo add --force-update ' + mylogin + newvals['helmrepo']['name'] + " " + newvals['helmrepo']['url'])
         # runcmd(fp_task, to_dir, helm_exe + ' repo add ' + mylogin + newvals['helmrepo']['name'] + " " + newvals['helmrepo']['url'])
         runcmd(fp_task, to_dir, helm_exe + ' repo update')
 
