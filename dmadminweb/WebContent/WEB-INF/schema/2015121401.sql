1e22e807fa63b12c5191d28d232d731ff5382d4e
Steve Taylor <steve@deployhub.com>
2021-10-12 15:28:14 -0600

    add license headers Signed-off-by: Steve Taylor <steve@deployhub.com>

--- dmadminweb/WebContent/WEB-INF/schema/2015121401.sql
+++ dmadminweb/WebContent/WEB-INF/schema/2015121401.sql
@@ -1,3 +1,17 @@
+-- Copyright (c) 2021 Linux Foundation
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 create table dm.dm_taskaction(id integer,actionid integer);
 insert into dm.dm_propertydef(defid,name,required,appendable) values(3,'SSL','N','N');
 insert into dm.dm_plugin(id,version,library) values(6,10000,'restful');
